9757c1c56fb0c6870bf2db586b4f94c82b20bf78
Priscilla <plie@student.unimelb.edu.au>
2019-08-12 21:55:07 +1000

    add mongodb using mongoose

--- app.js
+++ app.js
@@ -22,6 +22,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+// Setup Database (work in progress)
+require('./models/db.js');
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
---
+++ models/db.js
@@ -0,0 +1,26 @@
+const mongoose =  require('mongoose');
+
+
+
+
+//copy from CONNECT (MongoDB Atlas)
+const dbURI =
+    "mongodb+srv://priscilla:A9qiVFZSiqjFhfgm@cluster0-guonz.mongodb.net/test?retryWrites=true&w=majority";
+
+
+
+const options = {
+    useNewUrlParser: true,
+    dbName: "ProjectInheritDB"
+};
+
+mongoose.connect(dbURI, options).then(
+    () => {
+        console.log("Database connection established!");
+    },
+    err => {
+        console.log("Error connecting Database instance due to: ", err);
+    }
+);
+
+// require('./users.js');
