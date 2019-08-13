6ef3c4101bfb43a925aa519f6c155b4555eded17
Priscilla <plie@student.unimelb.edu.au>
2019-08-13 11:28:08 +1000

    database connected

--- models/db.js
+++ models/db.js
@@ -34,45 +34,4 @@ mongoose.connect(dbURI, options).then(
     }
 );
 
-
-
-
-
-/*
-// Import the native MongoDB driver
-const Mongo = require('mongodb')
-// Import Winston for async logging
-const Winston = require('winston')
-// The MongoClient class provides an interface for connecting to a // MondoDB database
-const MongoClient = Mongo.MongoClient
-// Url of your MongoDB database. Substitute with your own
-const dbURI = 'mongodb+srv://priscilla:A9qiVFZSiqjFhfgm@cluster0-guonz.mongodb.net/test?retryWrites=true&w=majority'
-// Call the client comment method and provide a callback to report // connection results
-MongoClient.connect(dbURI, (err, db) => {
-    // If the connection failed, report the error and return
-    if (err)
-        return Winston.error(`Unable to connect to server: ${err}`)
-
-    // The connection worked, let's log that too
-    Winston.info(`Connected to MongoDB database at ${dbURI}`)
-    // This is where we'll do our work against the db
-    // When we're done, make sure to close the connection
-    db.close()
-});
-
-
-
-
-
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://priscilla:A9qiVFZSiqjFhfgm@cluster0-guonz.mongodb.net/test?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
-});
-
-*/
-
 require('./user.js');
