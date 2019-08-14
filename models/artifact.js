5e051b9a08d55081a7b91182ee6d404961b3c710
kaylenesivanandam <ksivanandam@student.unimelb.edu.au>
2019-08-14 10:46:05 +1000

    artifact schema complete

---
+++ models/artifact.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose');
+
+var mongoose = require('mongoose');
+var artifactSchema = mongoose.Schema(
+    {
+        "id": String,
+        "title":String,
+        "description":String,
+        "photo":String,
+        "owner":String,
+        "category":String,
+        "created":Date
+    }
+);
+module.exports = mongoose.model('Artifact', artifactSchema);
