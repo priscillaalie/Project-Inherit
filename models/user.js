ae1458cee2e7202734944f15bc54763807770e04
Priscilla <plie@student.unimelb.edu.au>
2019-08-16 15:27:20 +1000

    password hashing might work

--- models/user.js
+++ models/user.js
@@ -1,6 +1,8 @@
 var mongoose = require('mongoose');
 var uniqueValidator = require('mongoose-unique-validator');
+var crypto = require('crypto');
 
+// defining user schema
 var userSchema = new mongoose.Schema({
 
     email: {
@@ -28,6 +30,17 @@ var userSchema = new mongoose.Schema({
 
 userSchema.plugin(uniqueValidator, {message: 'is already taken.'});
 
+
+// password hashing
+userSchema.methods.setPassword = function(password){
+    this.salt = crypto.randomBytes(16).toString('hex');
+    this.hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex');
+    };
+
+userSchema.methods.validPassword = function(password) {
+    var hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex');
+    return this.hash === hash;
+    };
 
 
 const User = mongoose.model('User', userSchema);
