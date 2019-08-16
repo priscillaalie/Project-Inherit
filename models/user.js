a9eb591b4ca56d8d87fe16879a2a2a840ea8d62f
Priscilla <plie@student.unimelb.edu.au>
2019-08-16 15:18:40 +1000

    adding email validation

--- models/user.js
+++ models/user.js
@@ -1,12 +1,17 @@
 var mongoose = require('mongoose');
+var uniqueValidator = require('mongoose-unique-validator');
 
 var userSchema = new mongoose.Schema({
+
     email: {
         type: String,
-        required: true,
-        trim: true,
+        lowercase: true,
         unique: true,
+        required: [true, "can't be blank"],
+        match: [/\S+@\S+\.\S+/, 'is invalid'],
+        index: true
     },
+
     name: {
         type: String,
         required: true,
@@ -17,7 +22,13 @@ var userSchema = new mongoose.Schema({
         required: true,
         hash: String,
     }
-});
+
+
+}, {timestamps: true});
+
+userSchema.plugin(uniqueValidator, {message: 'is already taken.'});
+
+
 
 const User = mongoose.model('User', userSchema);
 module.exports = User;
