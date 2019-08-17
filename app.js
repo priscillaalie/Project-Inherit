abbaa0703939928a634c78b48424afcfd30ea193
Priscilla <plie@student.unimelb.edu.au>
2019-08-17 19:50:52 +1000

    update app.js

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 // view engine setup
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
@@ -22,23 +23,23 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// Setup Database (work in progress)
+// Connect to Database
 require('./models/db.js');
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 
@@ -46,7 +47,7 @@ app.use(function(err, req, res, next) {
 const PORT = process.env.PORT || 3000;
 // app loading on server
 app.listen(PORT, function(){
-  console.log(`Express listening on port ${PORT}`);
+    console.log(`Express listening on port ${PORT}`);
 });
 
 module.exports = app;
