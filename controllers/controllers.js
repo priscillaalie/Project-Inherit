4ffeb8adcd2900dfbc3c5ec33ccb7ffb905535d6
Priscilla <plie@student.unimelb.edu.au>
2019-08-19 14:41:56 +1000

    creating signup view

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,8 +1,23 @@
-const express = require('express');
-const app = express();
+const mongoose = require('mongoose');
+const Artifact = require('../models/artifact');
+const User = require('../models/user');
+
+var  showIndex = function(req,res) {
+    var results = {title: 'Inherit', error: " "};
+    res.render('index', results);
+};
+
+module.exports = {
+    showIndex
+};
 
 var Login = function (req,res) {
     res.render('login.pug', {title: 'Login'});
 };
 
+var Signup = function (req,res) {
+    res.render('signup.pug', {title: 'Signup'});
+};
+
 module.exports.fetchLogin = Login;
+module.exports.fetchSignup = Signup;
